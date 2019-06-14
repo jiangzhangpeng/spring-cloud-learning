15787ee6b9321c0cac0f1c222fdbf7b0c04ec4a2
mangoslug <jiangzp_beijing@163.com>
2019-06-14 17:16:19 +0800

    20190614
    20190614

--- spring-cloud-oath2-jwt/auth/src/main/java/cn/com/clumsybear/security/utils/JsonUtil.java
+++
@@ -1,9 +0,0 @@
-package cn.com.clumsybear.security.utils;
-
-import com.alibaba.fastjson.JSON;
-
-public class JsonUtil {
-    public static String toJson(Object object){
-      return JSON.toJSONString(object);
-    }
-}
